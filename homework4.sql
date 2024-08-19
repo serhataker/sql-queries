1efb3c7379e8a836e9fbd99ae5921e0db28ed80c
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-19 18:22:03 +0300

    Delete homework4.sql

--- homework4.sql
+++
@@ -1,6 +0,0 @@
-select distinct replacement_cost from film;-- question 1
-select count(distinct replacement_cost) from film;-- question 2
-select count (title) as Film_Count from film where title like'A%' and rating='G'--question 3 
-select count(country) from country where length(country)=5;--question 4
-select count(*) from city where city ilike '%r';--question 4
-
