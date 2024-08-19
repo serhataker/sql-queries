0d0ebc29897e3d464fcdd5d87e561c078e5f70d0
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-19 14:49:55 +0300

    Delete homework2.sql

--- homework2.sql
+++
@@ -1,3 +0,0 @@
-select * from film where replacement_cost between 12.99 and 16.99 --quesiton 1
-select first_name,last_name from actor  where first_name in('Penelope','Nick','Ed') --question 2
-select * from film where rental_rate in(0.99, 2.99, 4.99) and replacement_cost in(12.99, 15.99, 28.99) --quesiton 3 
