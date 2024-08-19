eb3322f250f82f31f4274b7d5b361f14a055a6ea
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-19 18:22:11 +0300

    Delete homework5.sql

--- homework5.sql
+++
@@ -1,3 +0,0 @@
-Select * from film where title like'%n' order by length desc limit 5; --question 1 
-Select * from film where title like'%n' order by length asc offset 5 limit 5; --question 2 
-Select * from customer where store_id=1  order by length(last_name) desc limit 4; --question 3 
