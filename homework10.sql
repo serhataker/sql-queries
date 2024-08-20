a6747162227c4957cd81c976401ad3e2244ca75d
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-08-20 16:50:31 +0300

    Add files via upload

---
+++ homework10.sql
@@ -0,0 +1,15 @@
+--Question 1
+select ci.city as Şehir,Co.Country as Ülke from City Ci
+left join country Co
+on Ci.country_id=Co.country_id;
+
+--Question 2
+select pa.payment_id as ödeme_id,cu.first_name as Müşteri_Adı,cu.Last_Name as Müşteri_Soyadı  from customer cu
+right join payment pa
+on cu.customer_id=pa.customer_id
+
+
+--Question 3
+select r.rental_id  as Kira_No,cu.first_name as Müşteri_Adı,cu.Last_Name as Müşteri_Soyadı  from customer cu
+full join rental r
+on cu.customer_id=r.customer_id
